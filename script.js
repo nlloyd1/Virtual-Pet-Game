fb2bb5d1ae495dc7b38ca3d28412e42af4a6125c
Natalie Lloyd <55217875+nlloyd1@users.noreply.github.com>
2021-08-20 22:51:13 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/**
+ * When "hatch" button clicked, Takes user to main game page
+ */
+function hatchEgg() {
+    window.location.href = 'pet-store.html';
+}
+/**
+ * Puts red box around selected egg, stores in temp variable
+ */
+function selectEgg() {
+
+}
+/**
+ * Save name and selected egg as chosen pet
+ */
+function adoptPet() {
+
+}
